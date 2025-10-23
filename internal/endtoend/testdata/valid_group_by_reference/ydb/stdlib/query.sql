b0c78640fe3230400b20e6792a3f86b185034555
Viktor Pentyukhov <vipentyukhov@edu.hse.ru>
2025-10-23 19:31:09 +0300

    fixed test

--- internal/endtoend/testdata/valid_group_by_reference/ydb/stdlib/query.sql
+++ internal/endtoend/testdata/valid_group_by_reference/ydb/stdlib/query.sql
@@ -7,8 +7,9 @@ SELECT id, name AS name, bio
 FROM authors;
 
 -- name: ListMetrics :many
-SELECT DateTime::Format("%Y-%m-%d", time) AS bucket, city_name, Avg(temp_c)
+SELECT DateTime::Format("%Y-%m-%d")(time) AS bucket, city_name, Avg(temp_c)
 FROM weather_metrics
-WHERE time > DateTime::Now() - Interval("P6M")
+WHERE time > DateTime::MakeTimestamp(DateTime::Now()) - Interval("P6M")
 GROUP BY bucket, city_name
 ORDER BY bucket DESC;
+
