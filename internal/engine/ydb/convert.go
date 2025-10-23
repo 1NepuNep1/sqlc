94015119930afafa0a66cc04a197b48aa36786c2
Viktor Pentyukhov <vipentyukhov@edu.hse.ru>
2025-10-23 18:49:53 +0300

    fixed incorrect func name

--- internal/engine/ydb/convert.go
+++ internal/engine/ydb/convert.go
@@ -3323,9 +3323,7 @@ func (c *cc) handleInvokeSuffix(base ast.Node, invokeCtx *parser.Invoke_exprCont
 	case *ast.FuncCall:
 		funcCall.Func = baseNode.Func
 
-		if c.isLambdaFunction(baseNode.Func.Name) {
-			funcCall.Func.Name = baseNode.Func.Name + "_call"
-		}
+		funcCall.Func.Name = baseNode.Func.Name + "_call"
 
 		funcCall.Funcname.Items = append(funcCall.Funcname.Items, &ast.String{Str: funcCall.Func.Name})
 		return funcCall
